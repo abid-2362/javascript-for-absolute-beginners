92cedd7f94d4cbe2a6b8db6ac4cba9daad24487a
Abid Ali <abdsoftfsd@gmail.com>
2021-04-29 16:27:03 +0500

    Lecture 2 completed

--- js/01_basic.js
+++
@@ -1,1 +0,0 @@
-alert("Congratulations on your first javascaript program...");
---
+++ js/script.js
@@ -0,0 +1,27 @@
+// variables and constants
+
+// Assigning the values to variables
+let userName = "Abid Ali";
+
+
+// assigning the values to the constants
+const pi = 3.14;
+
+// userName;
+
+/*
+ye 1 multi line comment He
+is mein 1 se ziada lines hein
+*/
+
+
+// accessing the variable values
+alert(userName);
+
+// Changing the values in variables - allowed
+userName = "Abid";
+
+// changing the values in the constatns - NOT ALLOWED
+// pi = 3.15
+
+alert(userName);
