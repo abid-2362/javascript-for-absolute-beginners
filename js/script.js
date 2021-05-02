4e02cc433152d1d2378b72455ff89d06a65aee40
Abid Ali <abdsoftfsd@gmail.com>
2021-05-02 11:54:36 +0500

    Completes Tutorial 5

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
+// JavaScript has multiple data types
 // Data Types in JavaScript
 
-// JavaScript has multiple data types
 
 /*
     String      -> any text data
@@ -41,3 +41,74 @@ let isValid = false;
 // undefined and null data
 let age = undefined;
 let workHours = null;
+
+// accessing values
+/* console.log(priceOfItem);
+
+console.log('employee details', employeeDetails);
+
+console.log('employee name', employeeDetails.name);
+
+console.log('employee last name', employeeDetails.lastName);
+ */
+
+// reassigning or overwriting values in variables
+// isValid = true;
+// console.log('isValid: ',isValid);
+// console.log(employeeDetails.name);
+// employeeDetails.name = "Ahsan Ali";
+// console.log('employee name: ',employeeDetails.name);
+
+// Basic JavaScript Operators
+
+/* 
+    Arithmetic operators
+    + - * / %
+
+    Assignment Operator
+    =
+
+    Comparison Operators
+    <
+    <=
+    >
+    >=
+    ==
+    ===
+*/
+
+let number1 = 11;
+let number2 = 5;
+
+// addition
+// console.log(number1 + number2);
+
+// // subtraction
+// console.log(number1 - number2);
+
+// // division
+// console.log(number1 / number2);
+
+// // multiplication
+// console.log(number1 * number2);
+
+// // modulo operator
+// console.log(number1 % number2); // 1
+
+// comparison operators
+console.log(number1 === number2);
+
+// excercise
+/* 
+    variables number1 aur number2 mein values
+    tabdeel karein aur doosre comparion
+    operators laga kar console mein result
+    check karein.
+
+    hint:
+    console.log(number1 < number2);
+    and so on...
+ */
+
+let remainder = number1 % number2;
+console.log(remainder);
